e7039849de5f0342bb5c58c1efc6dc70fb69f60c
theogirl <theogirl@outlook.com>
2015-04-23 16:39:57 -0400

    Finished X challenge

--- js/app.js
+++ js/app.js
@@ -28,7 +28,9 @@ $(document).ready (function() {
 		$('.ryu-ready').show();
 		//stop sound
 
-	});
+	})
+
+});
 
 function playHadouken() {
 	$('#hadouken-sound') [0].volume = 0.5;
@@ -36,9 +38,20 @@ function playHadouken() {
 	$('#hadouken-sound') [0].play();
 }
 
+//Attach the keydown event to document as a whole, since can happen anywhere!
+$(document)
+.keydown(function(event) {
+	if (event.which == 88) {
+		$('.ryu-still').hide();
+		$('.ryu-ready').hide();
+		$('.ryu-throw').hide();
+		$('.ryu-cool').show();
+	};
+})
 
-
-
-
+//No need to listen for which specific key was released?
+.keyup(function() {
+		$('.ryu-still').show();
+		$('.ryu-cool').hide();
 
 });
